419bf49c92e64b701f0371e4fdd58171e04921f2
Madhumitha <madhumithavenu03@gmail.com>
2023-10-12 11:40:58 +0530

    creating js file

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Welcome to Tic Tac Toe")
+let music = new Audio("music.mp3")
+let audioTurn = new Audio("ting.mp3")
+let gameover = new Audio("gameover.mp3")
+let turn = "X"
+let isgameover = false;
+
+// Function to change the turn
+const changeTurn = ()=>{
+    return turn === "X"? "0": "X"
+}
+
